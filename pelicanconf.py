fd8cc53b9572fa9f640e0ba66473803cf8ddbc07
niki <s.niki.326@gmail.com>
2017-11-29 01:49:32 +0900

    adopt theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,6 +32,6 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 #THEME
-THEME = 'themes/mg'
+THEME = '/Users/NIKI/pelican-themes/mg'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
